7a351bf3e99d9f3aecaabfa8ca071a007c29ff4d
peelingtheonion <xxxfutures@gmail.com>
2016-09-03 11:47:41 +0800

    Js练习

--- JavaScript.js
+++ JavaScript.js
@@ -19,3 +19,21 @@ func2('some text');
 func3(1,2,func4());
 alert(func4());
 alert(func5);
+
+
+//function
+//(...args)代表任意数量的数组
+var func3 = function(...args){
+//args调用function(...args)
+    console.log(args,func5());
+    console.log('args.length = ',args.length);
+}
+var func4 = new Function(return 3)
+function func5(){
+    return 4;
+}
+
+func3(1,8,99,"at",func4());
+//Console中执行后返回的是 [1,8,99,"at",3] 4 args.length = 5
+
+
--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 #vol1-onion
-***
 ##一些参考文档在线资源
-***
 *JavaScript 教程
 *Node.js 文档
 *Express 文档
@@ -9,12 +7,9 @@
 *Git 文档
 
 ##工具下载
-***
 *VirtualBox 虚拟机
 	-VirtualBox platform pacdages
 	-VirtualBox 5.14 Oracla VM VirtualBox Extension Pack
 *Atom 编辑器
 
 ##注册虚拟机
-
-
