1552abec2a148291dead05511045d7f04bda380b
peelingtheonion <xxxfutures@gmail.com>
2016-09-23 20:31:07 +0800

    lesson_7

---
+++ lesson_7/lib/routers/wxapi.js
@@ -0,0 +1,10 @@
+var router = require('express').Router(),
+    xml = require('xml'),
+    xmlBodyParser = require('express-xml-bodyparser');
+
+router.route('/')
+    .get(function(req,res,next){
+        res.send('hahahah');
+    });
+
+module.exports = router;
